d70d78510ac6352607842196e00fcc8c37570163
gbtrajano <gbtrajano1@gmail.com>
2025-07-17 19:58:18 -0300

    Some updates

---
+++ src/app/Components/Trial/Trial.jsx
@@ -0,0 +1,25 @@
+const Trial = () => {
+  return (
+    <section class="bg-white dark:bg-gray-900">
+      <div class="py-8 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6">
+        <div class="mx-auto max-w-screen-sm text-center">
+          <h2 class="mb-4 text-4xl tracking-tight font-extrabold leading-tight text-gray-900 dark:text-white">
+            Inicie seu teste grátis hoje!
+          </h2>
+          <p class="mb-6 font-light text-gray-500 dark:text-gray-400 md:text-lg">
+            Ficou na dúvida? Daremos a oportunidade de você ter seu Site ou
+            Sistema gratuitamente por 1 mês, Sem pagar nada.
+          </p>
+          <a
+            href="#"
+            class="text-white bg-[#f03612] hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-bold rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800"
+          >
+            Teste grátis de 1 mês.
+          </a>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Trial;
--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,17 @@
+import Companies from "./Components/Companies/Companies";
 import Hero from "./Components/hero/Hero";
+import Info from "./Components/Info/Info";
 import Maintenance from "./Components/Maintenance/Maintenance";
+import Trial from "./Components/Trial/Trial";
 
 const page = () => {
   return (
     <div>
-      {/* <Hero /> */}
-      <Maintenance />
+      <Hero />
+      {/* <Maintenance /> */}
+      <Info />
+      <Companies />
+      <Trial />
     </div>
   );
 };
